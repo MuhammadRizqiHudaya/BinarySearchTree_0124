3228019a2df6517b4b7988f55478f631a1fc4729
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-26 15:31:43 +0700

    void insert

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -42,4 +42,56 @@ public:
         }
     }
 
+   void insert()
+    {
+        int x;
+        cout << "masukkan nilai:";
+        cin >> x;
+
+        //step 1 allcoate memory for thge new node
+        Node*newNode = new Node;
+
+        //step 2 assign value to the data
+        newNode->info = x;
+
+        //step 3 make the left and right child of the new node null
+        newNode->leftchild = nullptr; 
+        newNode->rightchild= nullptr; 
+
+        // step 4 locate the node which will be the parent of the new node
+        Node*parent = nullptr;
+        Node*currentNode = nullptr;
+        search(x, parent, currentNode);
+
+        //step 5 if parent is NULL
+        if (parent == nullptr)
+        {
+            //5a Mark the new node as ROOT
+            ROOT = newNode;
+
+            //5b exit
+            return;
+        }
+
+        //step 6 if the value in the data field of the new node is less than that of parent
+        if (x < parent->info)
+        {
+            //6a make the left child of the parent point to the new node
+            parent->leftchild = newNode;
+
+            //6b exit
+            return;
+        }
+
+        //step 7 if the value in the data field of the new node is greater than that of parent
+        else if (x > parent->info)
+        {
+            //7a make the right child of the parent point to the new node
+            parent->rightchild = newNode;
+
+            //7b exit 
+            return;
+        }
+    }
+
     
