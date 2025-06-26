d2ef941fe6783c55127962f798d6fbbf36ba5674
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-26 15:33:13 +0700

    void inorder

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -100,4 +100,18 @@ public:
         return ROOT ==nullptr;
     }
 
-    
+    void inorder(Node *ptr)
+    {
+        if(isEmpty())
+        {
+            cout << "tree is empty" << endl;
+            return;
+        }
+        if (ptr == nullptr)
+        return;
+
+        inorder(ptr->leftchild);
+        cout << ptr->info << "";//parent
+        inorder(ptr->rightchild);
+    }
+};    
